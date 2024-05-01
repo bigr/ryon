fc7880867ac04c76e62944c7e612963a630f766c
Pavel Klinger <ja@bigr.cz>
2024-05-01 22:14:11 +0200

    :white_check_mark: Improve parse tests

--- tests/unit/parser/parser_test.py
+++ tests/unit/parser/parser_test.py
@@ -1,10 +1,17 @@
+import pytest
+
 from ryon.parser import RyonParser
+from ryon.parser.yaml_dumper import ast_to_yaml
+from tests.data.code_fragments import fragments
 
 
 def test_parser_initialization():
     RyonParser()
 
 
-def test_parser_parse(parser, tests_data_dir):
-    content = open(tests_data_dir / "foo.ry").read()
-    parser.parse(content)
+@pytest.mark.parametrize("fragment", fragments)
+def test_parser(parser, fragment):
+    ast = parser.parse(fragment.code)
+    actual = ast_to_yaml(ast)
+
+    assert actual == fragment.ast, f"Failed parsing {fragment.name}: {fragment.code}"
