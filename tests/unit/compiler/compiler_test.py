58a1797aabbfe20d579cd31216896d65a6a2be94
Pavel Klinger <ja@bigr.cz>
2024-05-06 08:41:23 +0200

    :sparkles: Compiler

--- tests/unit/compiler/compiler_test.py
+++ tests/unit/compiler/compiler_test.py
@@ -1,5 +1,29 @@
+import pytest
+from ryon.hlir.yaml_loader import yaml_to_hlir
+
 from ryon.compiler import RyonCompiler
+from tests.data.code_fragments import fragments
+
+import llvmlite.binding as llvm
+
+
+@pytest.fixture
+def init_llvm():
+    llvm.initialize()
+    llvm.initialize_native_target()
+    llvm.initialize_native_asmprinter()
 
 
 def test_compiler_initialization():
     RyonCompiler()
+
+
+@pytest.mark.parametrize("fragment", fragments)
+def test_compiler(compiler, fragment):
+    hlir = yaml_to_hlir(fragment.hlir)
+
+    llvm_ir = compiler.visit(hlir)
+
+    actual = llvm_ir[llvm_ir.find('target datalayout = ""') + 22 :].strip()
+
+    assert actual == fragment.llvm_ir
