e736d0869b7c8234d5010b3b78b452f70e76fe5f
Pavel Klinger <ja@bigr.cz>
2024-05-06 08:41:23 +0200

    :sparkles: Compiler

--- ryon/compiler/compiler.py
+++ ryon/compiler/compiler.py
@@ -1,17 +1,51 @@
-from lark import Visitor
-import llvmlite.binding as llvm
 from llvmlite import ir
+
+from ryon.hlir.visitor import Visitor
 
 
 class RyonCompiler(Visitor):
     def __init__(self):
-        self.ir_module = ir.Module(name="main_module")
-        self.ir_module.triple = llvm.get_default_triple()
-        self.builder = None
+        pass
 
-    def module(self, node):
-        func_type = ir.FunctionType(ir.VoidType(), [])
-        main_function = ir.Function(self.ir_module, func_type, name="main")
-        block = main_function.append_basic_block(name="entry")
-        self.builder = ir.IRBuilder(block)
-        self.builder.ret_void()
+    def module(self, node, parent_data):
+        module = ir.Module(name="simple_module")
+        yield module
+        yield str(module)
+
+    def fn(self, node, module):
+        function_type = ir.FunctionType(ir.IntType(32), [ir.IntType(32)] * len(node.args))
+        function = ir.Function(module, function_type, name=node.name)
+        for arg, node_arg in zip(function.args, node.args):
+            arg.name = node_arg.name
+        yield function
+
+    def suite(self, node, function):
+        block = function.append_basic_block(name="entry")
+        builder = ir.IRBuilder(block)
+        yield builder, function
+
+    def return_(self, node, parent_data):
+        builder, function = parent_data
+
+        child_nodes = yield builder, function
+        expression = child_nodes["expression"]
+        builder.ret(expression)
+
+    def summation(self, node, parent_data):
+        builder, function = parent_data
+        child_nodes = yield builder, function
+        addends = child_nodes["addends"]
+        sum_result = addends[0]
+        for addend in addends[1:]:
+            sum_result = builder.add(sum_result, addend, name="result")
+        yield sum_result
+
+    def var(self, node, parent_data):
+        builder, function = parent_data
+        yield
+        yield next(arg for arg in function.args if arg.name == node.name)
+
+    def decimal_number(self, node, parent_data):
+        builder, _ = parent_data
+        yield
+        yield ir.Constant(ir.IntType(32), int(node.value))
--- tests/data/code_fragments.py
+++ tests/data/code_fragments.py
@@ -8,12 +8,14 @@ class Fragment:
     code: str = field(repr=False)
     ast: str = field(repr=False)
     hlir: str = field(repr=False)
+    llvm_ir: str = field(repr=False)
 
     def __post_init__(self):
         # Using object.__setattr__ to bypass the immutability for initialization
         object.__setattr__(self, "code", textwrap.dedent(self.code).strip() + "\n")
         object.__setattr__(self, "ast", textwrap.dedent(self.ast).strip() + "\n")
         object.__setattr__(self, "hlir", textwrap.dedent(self.hlir).strip() + "\n")
+        object.__setattr__(self, "llvm_ir", textwrap.dedent(self.llvm_ir).strip())
 
 
 fragments = [
@@ -57,7 +59,14 @@ fragments = [
                     - !node.Return
                         expression: !node.DecimalNumber
                             value: 10
-       """,
+        """,
+        llvm_ir="""
+            define i32 @"hello_world"()
+            {
+            entry:
+              ret i32 10
+            }
+        """,
     ),
     Fragment(
         name="Add function",
@@ -144,6 +153,15 @@ fragments = [
                                 type: null
                             - !node.DecimalNumber
                                 value: 5
+        """,
+        llvm_ir="""
+            define i32 @"add"(i32 %"a", i32 %"b")
+            {
+            entry:
+              %"result" = add i32 %"a", %"b"
+              %"result.1" = add i32 %"result", 5
+              ret i32 %"result.1"
+            }
         """,
     ),
 ]
